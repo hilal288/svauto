7344fc938a475e45b8c867fcf71d92ed5749760f
Thiago Martins <tmartins@sandvine.com>
2016-07-21 00:37:45 +0000

    Removing *_auto_config out from image-factory.sh

--- image-factory.sh
+++ image-factory.sh
@@ -356,7 +356,7 @@ fi
 EXTRA_VARS="base_os="$BASE_OS" release="$RELEASE" activate_eth1="no" deployment_mode="yes""
 
 if [ "$DISABLE_AUTOCONF" == "yes" ] ; then
-	EXTRA_VARS="$EXTRA_VARS disable_autoconf="yes" sv_auto_config="no" cs_auto_config="no""
+	EXTRA_VARS="$EXTRA_VARS disable_autoconf="yes""
 fi
 
 if [ "$STATIC_REPO" == "yes" ] ; then
