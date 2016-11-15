8123a2ef72b168ac15392e9836459570c6a85202
Thiago Martins <tmartins@sandvine.com>
2016-11-15 01:04:39 +0000

    Updating Heat Templates / runcmd & helpers
    Adjusting it to the new "--ansible-inventory-builder" and
    "--ansible-playbook-builder", not it works!
    
    Also, small update on ubuntu-network-setup...

--- scripts/bootstrap-xenial-server.sh
+++ scripts/bootstrap-xenial-server.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-curl -s https://raw.githubusercontent.com/sandvine-eng/svauto/dev/scripts/svauto-deployments.sh | bash -s -- --base-os=ubuntu16 --ansible-run-against="local" --ansible-remote-user="root" --ansible-inventory-builder="all,localhost,ansible_connection=local,regular_system_user=ubuntu,base_os=ubuntu16,os_release=newton" --ansible-playbook-builder="localhost,bootstrap;base_os_upgrade=yes;ubuntu_install=server,grub-conf,ssh_keypair,hyper_kvm,os_clients,post-cleanup"
+curl -s https://raw.githubusercontent.com/sandvine-eng/svauto/dev/scripts/svauto-deployments.sh | bash -s -- --base-os=ubuntu16 --ansible-run-against="local" --ansible-remote-user="root" --ansible-inventory-builder="all,localhost,ansible_connection=local,regular_system_user=ubuntu,base_os=ubuntu16,ubuntu_install=server,os_release=newton" --ansible-playbook-builder="localhost,bootstrap;base_os_upgrade=yes,grub-conf,ssh_keypair,hyper_kvm,os_clients,post-cleanup"
