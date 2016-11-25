07906db68cc9b29e6e52833f5cfc623d6dc2dcd5
Thiago Martins <tmartins@sandvine.com>
2016-11-25 10:56:41 -0500

    Adding more Desktop tools for Ubuntu!
    * Teamviewer;
    * Skype Alpha;
    * NoMachine (Remote Desktop via SSH).

--- scripts/preset-xenial-desktop.sh
+++ scripts/preset-xenial-desktop.sh
@@ -3,4 +3,4 @@
 ./svauto.sh --base-os=ubuntu16 \
 	--ansible-remote-user="root" \
 	--ansible-inventory-builder="all,localhost,ansible_connection=local,base_os=ubuntu16,ubuntu_install=desktop,os_release=newton" \
-	--ansible-playbook-builder="localhost,bootstrap;base_os_upgrade=yes,grub-conf,ubuntu-desktop,ssh_keypair,hyper_kvm,openvswitch;openvswitch_mode=regular,hyper_virtualbox,vagrant,os_clients,google-chrome,mattermost,sublime,ccollab-client,post-cleanup"
+	--ansible-playbook-builder="localhost,bootstrap;base_os_upgrade=yes,grub-conf,ubuntu-desktop,ssh_keypair,hyper_kvm,openvswitch;openvswitch_mode=regular,hyper_virtualbox,vagrant,os_clients,google-chrome,mattermost,sublime,skype,teamviewer,nomachine,ccollab-client,post-cleanup"
