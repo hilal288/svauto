e35b19eecf0253becb6f1019f0b50911573047b4
Thiago Martins <tmartins@sandvine.com>
2016-11-25 20:13:01 -0500

    Wrong Ubuntu install mode

--- scripts/preset-xenial-server.sh
+++ scripts/preset-xenial-server.sh
@@ -2,5 +2,5 @@
 
 ./svauto.sh --base-os=ubuntu16 \
 	--ansible-remote-user="root" \
-	--ansible-inventory-builder="all,localhost,ansible_connection=local,base_os=ubuntu16,ubuntu_install=desktop,os_release=newton" \
+	--ansible-inventory-builder="all,localhost,ansible_connection=local,base_os=ubuntu16,ubuntu_install=server,os_release=newton" \
 	--ansible-playbook-builder="localhost,bootstrap;base_os_upgrade=yes,grub-conf,ssh_keypair,hyper_kvm,openvswitch;openvswitch_mode=regular,vagrant,os_clients,post-cleanup"
