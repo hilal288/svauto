31bcf9edd8c68c7946029861aee5966ef3f59455
Thiago Martins <tmartins@sandvine.com>
2016-12-10 03:13:23 +0000

    Updatint few preset scripts
    Removing obsolete --base-os=*, using Ansible's facts from now on.

--- scripts/preset-svauto-server.sh
+++ scripts/preset-svauto-server.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-./svauto --base-os=ubuntu16 \
-	--ansible-remote-user="root" \
+./svauto --ansible-remote-user="root" \
 	--ansible-inventory-builder="all,localhost,ansible_connection=local,ubuntu_install=server,os_release=newton" \
 	--ansible-playbook-builder="localhost,bootstrap;base_os_upgrade=yes,grub-conf,apache2,hyper_kvm,hyper_lxd,hyper_virtualbox,docker,vagrant,amazon_ec2_tools,redhat_tools_ubuntu,os_clients,packer,vsftpd,post-cleanup"
