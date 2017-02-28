8f620ae8d4e897e9324f42b390066dd355068a50
Thiago Martins <tmartins@sandvine.com>
2017-02-27 21:15:47 -0500

    Fixing SVAuto bootstrap preset scripts
    * Adding "download-images" role

--- scripts/preset-svauto-server.sh
+++ scripts/preset-svauto-server.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-./svauto --ansible-remote-user="root" \
-	--ansible-inventory-builder="all,localhost,ansible_connection=local,ubuntu_install=server,os_release=newton" \
-	--ansible-playbook-builder="localhost,bootstrap;base_os_upgrade=yes,grub-conf,apache2,hyper_kvm,hyper_lxd,hyper_virtualbox,docker,vagrant,amazon_ec2_tools,redhat_tools_ubuntu,os_clients,packer,vsftpd,post-cleanup"
+./svauto.sh --ansible-remote-user="root" \
+	--ansible-inventory-builder="all,localhost,ansible_connection=local,ubuntu_install=server,os_release=ocata" \
+	--ansible-playbook-builder="localhost,bootstrap;base_os_upgrade=yes,grub-conf,apache2,hyper_kvm,hyper_lxd,hyper_virtualbox,docker,vagrant,amazon_ec2_tools,redhat_tools_ubuntu,os_clients,packer,vsftpd,download-images;download_group=iso,post-cleanup"
