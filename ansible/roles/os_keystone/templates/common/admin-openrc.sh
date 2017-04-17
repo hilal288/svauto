08c4ef866ffdd9abe1929a365c83293e84bc3a31
Thiago Martins <tmartins@sandvine.com>
2017-04-17 17:10:03 +0000

    ERRATA: Renaming an Ansible variable
    From endpoint_admin_uri to endpoint_internal_uri.

--- ansible/roles/os_keystone/templates/common/admin-openrc.sh
+++ ansible/roles/os_keystone/templates/common/admin-openrc.sh
@@ -3,5 +3,5 @@ export OS_USER_DOMAIN_NAME=default
 export OS_PROJECT_NAME=admin
 export OS_USERNAME=admin
 export OS_PASSWORD={{ admin_password }}
-export OS_AUTH_URL={{ endpoint_admin_uri }}{{ identity_endpoint_internal }}:35357/v3
+export OS_AUTH_URL={{ endpoint_internal_uri }}{{ identity_endpoint_internal }}:35357/v3
 export OS_IDENTITY_API_VERSION=3
