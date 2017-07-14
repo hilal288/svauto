83c8e64289ec16963a374a71ad2a15df6bc18a78
Thiago Martins <tmartins@sandvine.com>
2017-07-14 14:54:37 -0400

    Simplifying Ubuntu preset scripts

--- scripts/preset-xenial-desktop.sh
+++ scripts/preset-xenial-desktop.sh
@@ -2,4 +2,4 @@
 
 ./svauto.sh --ansible-remote-user="root" \
 	--ansible-inventory-builder="all,localhost,ansible_connection=local,ubuntu_install=desktop,os_release=ocata" \
-	--ansible-playbook-builder="localhost,bootstrap;base_os_upgrade=yes,grub-conf,ubuntu-desktop,ssh_keypair,hyper_kvm,openvswitch;openvswitch_mode=regular,hyper_virtualbox,vagrant,os_clients,google-chrome,mattermost,sublime,skype,teamviewer,nomachine,ccollab-client,post-cleanup"
+	--ansible-playbook-builder="localhost,bootstrap;base_os_upgrade=yes,grub-conf,ubuntu-desktop,ssh_keypair,hyper_kvm,openvswitch;openvswitch_mode=regular,hyper_virtualbox,vagrant,os_clients,google-chrome,mattermost,sublime,skype,teamviewer,ccollab-client,post-cleanup"
--- scripts/preset-xenial-server.sh
+++ scripts/preset-xenial-server.sh
@@ -2,4 +2,4 @@
 
 ./svauto.sh --ansible-remote-user="root" \
 	--ansible-inventory-builder="all,localhost,ansible_connection=local,ubuntu_install=server,os_release=ocata" \
-	--ansible-playbook-builder="localhost,bootstrap;base_os_upgrade=yes,grub-conf,ssh_keypair,hyper_kvm,openvswitch;openvswitch_mode=regular,vagrant,os_clients,post-cleanup"
+	--ansible-playbook-builder="localhost,bootstrap;base_os_upgrade=yes,grub-conf,ssh_keypair,post-cleanup"
